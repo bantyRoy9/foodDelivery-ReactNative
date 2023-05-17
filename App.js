558e2b5787768467f5f45124a3d470fd9db6efea
Banti Kumar <banti.kumar@50hertz.in>
2023-05-17 15:19:49 +0530

    create basketScreen screen

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import HomeScreen from './Screens/HomeScreen';
 import Restaurent from './Screens/Restaurent';
 import { Provider } from 'react-redux';
 import { store } from './Store';
+import BasketScreen from './Screens/BasketScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +17,7 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name='Home' component={HomeScreen} options={{headerShown:false}}/>
           <Stack.Screen name='Restaurent' component={Restaurent} options={{headerShown:false}}/>
+          <Stack.Screen name="Basket" component={BasketScreen} options={{ presentation:"modal",headerShown:false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
--- Components/BasketIcons.jsx
+++ Components/BasketIcons.jsx
@@ -4,16 +4,18 @@ import PressableButton from './buttons/Pressable'
 import { useSelector } from 'react-redux'
 import { basketItemsTotal, selectBasketItems } from '../Feature/basketSlice'
 import { CurrencyDollarIcon, CurrencyRupeeIcon } from 'react-native-heroicons/outline'
+import { useNavigation } from '@react-navigation/native'
 
 const BasketIcons = () => {
 
     const item = useSelector(selectBasketItems);
     const basketTotal = useSelector(basketItemsTotal);
+    const navigation = useNavigation();
 
   return (
       <View className="absolute bottom-6 w-full z-50">
         {item.length ? <> 
-            <TouchableOpacity className="mx-5 bg-[#00CCBB] px-4 py-3 rounded-lg flex-row items-center space-x-1">
+            <TouchableOpacity onPress={()=>navigation.navigate('Basket')} className="mx-5 bg-[#00CCBB] px-4 py-3 rounded-lg flex-row items-center space-x-1">
                 <Text className="text-white font-extrabold text-lg bg-[#01a296] py-1 px-2">{item.length}</Text>
                 <Text className="flex-1 text-white text-lg font-extrabold text-center">View Basket</Text>
                 <Text className="text-lg text-white font-extrabold">
